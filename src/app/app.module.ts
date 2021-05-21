6059cfee9da6907daa1784e13505a99213b6e380
Zac Emoned <46713066+zac-dodol@users.noreply.github.com>
2021-05-21 18:40:35 +0800

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
